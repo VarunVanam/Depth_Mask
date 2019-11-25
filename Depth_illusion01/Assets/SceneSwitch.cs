646c59638db22484989bf80dc354a9e221bdcd55
VarunVanam <51377967+VarunVanam@users.noreply.github.com>
2019-11-25 11:27:15 +0530

    Added Proj
    Doesnt have room asset

---
+++ Depth_illusion01/Assets/SceneSwitch.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class SceneSwitch : MonoBehaviour
+{
+   public void GoUP()
+    {
+        SceneManager.LoadScene("01");
+    }
+
+    public void GoDown()
+    {
+        SceneManager.LoadScene("main");
+    }
+    
+}
